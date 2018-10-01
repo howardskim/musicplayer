1263da8580806228cab9629893e4e9696fff58a6
Howard Kim <howardkim820@gmail.com>
2018-10-01 15:08:00 -0700

    Fixed file pathing due to nested directory in portfolio

--- js/data/songs.js
+++ js/data/songs.js
@@ -1,62 +1,62 @@
 export const songList = [{
     "id": "1",
-    "url": "../../music/the_ringer.mp3",
+    "url": "./music/the_ringer.mp3",
     "time": "05:38",
     "title": "The Ringer",
     "artist": "Eminem"
   },
   {
     "id": "2",
-    "url": "../../music/fall.mp3",
+    "url": "./music/fall.mp3",
     "time": "04:22",
     "title": "Fall",
     "artist": "Eminem"
   },
   {
     "id": "3",
-    "url": "../../music/lucky_you.mp3",
+    "url": "./music/lucky_you.mp3",
     "time": "04:05",
     "title": "Lucky You",
     "artist": "Eminem ft. Joyner"
   },
   {
     "id": "4",
-    "url": "../../music/killshot.mp3",
+    "url": "./music/killshot.mp3",
     "time": "04:13",
     "title": "Killshot",
     "artist": "Eminem"
   },
   {
     "id": "5",
-    "url": "../../music/venom.mp3",
+    "url": "./music/venom.mp3",
     "time": "06:09",
     "title": "Venom",
     "artist": "Eminem"
   },
   {
     "id": "6",
-    "url": "../../music/normal.mp3",
+    "url": "./music/normal.mp3",
     "time": "03:41",
     "title": "Normal",
     "artist": "Eminem"
   },
   {
     "id": "7",
-    "url": "../../music/notalike.mp3",
+    "url": "./music/notalike.mp3",
     "time": "04:48",
     "title": "Not Alike",
     "artist": "Eminem"
   },
   {
     "id": "8",
-    "url": "../../music/goodguy.mp3",
+    "url": "./music/goodguy.mp3",
     "time": "02:22",
     "title": "Good Guy",
     "artist": "Eminem"
   },
   {
     "id": "9",
-    "url": "../../music/rapgod.mp3",
+    "url": "./music/rapgod.mp3",
     "time": "06:06",
     "title": "Rap God",
     "artist": "Eminem"
